ca7c4dafeb57ef4eff8aca1899a53d2635b009ab
Bruh-Beans <723536@student.dusd.net>
2024-03-12 11:00:38 -0700

    moeny

--- GonzalezPrototype-1P3/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ GonzalezPrototype-1P3/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,6 +7,7 @@ public class PlayerControllerX : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float tiltAngle = 3.0F;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +23,12 @@ public class PlayerControllerX : MonoBehaviour
 
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        transform.Rotate(Vector3.up,verticalInput * Time.deltaTime);
 
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        {
+            float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+            float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+            Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
+        }
     }
 }
---
+++ GonzalezPrototype-1P3/Assets/Scripts/test.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Vector3 velocity = Vector3.up;
+    public float smooth = 2.0F;
+    public float tiltAngle = 3.0F;
+    void Update()
+    {
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
+    }
+}
